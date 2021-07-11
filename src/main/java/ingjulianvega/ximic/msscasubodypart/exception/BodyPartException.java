0e0514a9594ef64e617193932be05d53223b3329
Julian Vega <ingjulianvega@gmail.com>
2021-07-11 18:22:01 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasubodypart/exception/BodyPartException.java
+++ src/main/java/ingjulianvega/ximic/msscasubodypart/exception/BodyPartException.java
@@ -1,15 +1,20 @@
 package ingjulianvega.ximic.msscasubodypart.exception;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BodyPartException extends RuntimeException {
 
-    private final String code;
+    private HttpStatus httpStatus;
+    private String apiCode;
+    private String error;
+    private String message;
+    private String solution;
 
-    public BodyPartException(final String code, final String message) {
-        super(message);
-        this.code = code;
-    }
 }
 
